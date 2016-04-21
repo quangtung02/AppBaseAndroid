d5a41414cc0dd99ae1c8dc3f8090d3040285e116
nguyen.quang.tung <nguyen.quang.tung@softfront.com.vn>
2016-04-21 13:26:15 +0700

    initial commit AppBaseAndroid

--- app/src/main/java/com/softfront/demo/MainActivity.java
+++ app/src/main/java/com/softfront/demo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.softfront.demo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -11,8 +13,10 @@ import com.softfront.demo.core.model.AppError;
 import com.softfront.demo.core.model.AppResponse;
 import com.softfront.demo.model.DataTest;
 import com.softfront.demo.until.DialogUtil;
+import com.softfront.demo.until.PermissionUntil;
 import com.softfront.demo.until.ToastUntil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
@@ -21,7 +25,11 @@ import retrofit2.Call;
 
 public class MainActivity extends Activity {
 
+    public static final int REQUEST_CODE_ASK_SINGLE_PERMISSION = 1;
+    public static final int REQUEST_CODE_ASK_MULTI_PERMISSION = 2;
+
     private Call mCallDataTest;
+    private List<String> listPermission = new ArrayList<String>();
 
     @Bind(R.id.progressBar_id) ProgressBar mpProgressBar;
     @Bind(R.id.text_view_id) TextView mTextId;
@@ -41,12 +49,64 @@ public class MainActivity extends Activity {
 
         // Action get data from server
         doGetData();
+
+        // TODO: 4/21/2016 this is sample code check permission. Now, no need import 
+        // Check single permission
+/*        if (PermissionUntil.checkSinglePermissionGranted(this, PermissionUntil.CAMERA,
+                REQUEST_CODE_ASK_SINGLE_PERMISSION, new PermissionUntil.ShowRationaleCallback() {
+                    @Override
+                    public void needShowRationale(final int requestCode) {
+                        showMessageOkCancel(getResources().getString(R.string.permission_access_msg) + " Camera ", new DatePickerDialog.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                PermissionUntil.createRequestSinglePermissions(MainActivity.this,
+                                        PermissionUntil.CAMERA, REQUEST_CODE_ASK_SINGLE_PERMISSION);
+                            }
+                        });
+                        return;
+                    }
+                })) ;
+
+        // Check Multiple permission
+        PermissionUntil.checkMultiplePermissionGranted(this, listPermission,
+                REQUEST_CODE_ASK_MULTI_PERMISSION, new PermissionUntil.ShowRationaleCallback() {
+                    @Override
+                    public void needShowRationale(int requestCode) {
+                        showMessageOkCancel(getResources().getString(R.string.permission_access_msg) + " " + listPermission.get(0),
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        PermissionUntil.createRequestMultiplePermissions(MainActivity.this,
+                                                listPermission, REQUEST_CODE_ASK_MULTI_PERMISSION);
+                                    }
+                                });
+                    }
+                });*/
     }
 
     @Override
     protected void onDestroy() {
-        //   if (mCallDataTest != null) mCallDataTest.cancel();
+        if (mCallDataTest != null) mCallDataTest.cancel();
         super.onDestroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_SINGLE_PERMISSION:
+                if (PermissionUntil.isGrantedForResult(grantResults)) {
+                    // TODO: 4/21/2016
+                } else {
+                    // TODO: 4/20/2016
+                }
+                break;
+            case REQUEST_CODE_ASK_MULTI_PERMISSION:
+                // TODO: 4/21/2016
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     /*Get data from server via API*/
@@ -77,7 +137,7 @@ public class MainActivity extends Activity {
 
     // Show data get from server to the screen
     private void showDataOnScreen(List<DataTest> dataTestList) {
-        /* Test one element */
+        /* Test for one element */
         DataTest dataTest = dataTestList.get(0);
         if (dataTest != null) {
             mTextId.setText(String.valueOf(dataTest.getId()).toString());
@@ -85,5 +145,14 @@ public class MainActivity extends Activity {
             mTextType.setText(String.valueOf(dataTest.isType()));
             mTextData.setText(dataTest.getData());
         }
+    }
+
+    public void showMessageOkCancel(String message, DialogInterface.OnClickListener clickListener) {
+        AlertDialog.Builder alBuilder = new AlertDialog.Builder(this);
+        alBuilder.setMessage(message);
+        alBuilder.setPositiveButton(getResources().getString(R.string.btn_ok), clickListener);
+        alBuilder.setNegativeButton(getResources().getString(R.string.btn_cancel), null);
+        alBuilder.create();
+        alBuilder.show();
     }
 }
--- app/src/main/java/com/softfront/demo/until/PermissionUntil.java
+++ app/src/main/java/com/softfront/demo/until/PermissionUntil.java
@@ -1,7 +1,93 @@
 package com.softfront.demo.until;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by nguyen.quang.tung on 4/21/2016.
  */
 public class PermissionUntil {
+
+    public static final String CAMERA = Manifest.permission.CAMERA;
+    public static final String READ_CONTACTS = Manifest.permission.READ_CONTACTS;
+    public static final String WRITE_CONTACTS = Manifest.permission.WRITE_CONTACTS;
+    public static final String[] listPermission = new String[]{CAMERA, READ_CONTACTS, WRITE_CONTACTS};
+
+
+    public interface ShowRationaleCallback {
+
+        //Call this method if we should show rationale
+        void needShowRationale(final int requestCode);
+    }
+
+    public static final boolean checkSinglePermissionGranted(final Activity activity, final String permission,
+                                                       final int requestCode,
+                                                       final ShowRationaleCallback showRationaleCallback) {
+        boolean isGranted = true;
+
+        if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+            isGranted = false;
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                if (showRationaleCallback != null)
+                    showRationaleCallback.needShowRationale(requestCode);
+            } else {
+                createRequestSinglePermissions(activity, permission, requestCode);
+            }
+        }
+        return isGranted;
+    }
+
+    public static void createRequestSinglePermissions(Activity activity, final String permission, int requestCode) {
+        ActivityCompat.requestPermissions(activity, new String[]{permission}, requestCode);
+    }
+
+    /*
+    * return true if granted
+    * */
+    public static boolean isGrantedForResult(int[] grantResults) {
+        return grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static void checkMultiplePermissionGranted(final Activity activity, final List<String> permissionList,
+                                               final int requestCode,
+                                               final ShowRationaleCallback showRationaleCallback) {
+        List<String> permissionNeed = new ArrayList<String>();
+
+        for (int i = 0; i < listPermission.length; i++) {
+            if (!addPermissionList(activity, permissionList, listPermission[i])) {
+                permissionNeed.add(listPermission[i]);
+            }
+        }
+
+        if (permissionList.size() > 0) {
+            if (permissionNeed.size() > 0) {
+                // Check rationale
+                if (showRationaleCallback != null)
+                    showRationaleCallback.needShowRationale(requestCode);
+            } else {
+                createRequestMultiplePermissions(activity, permissionList, requestCode);
+            }
+        }
+    }
+
+    public static void createRequestMultiplePermissions(Activity activity, final List<String> permissions, int requestCode) {
+        ActivityCompat.requestPermissions(activity, permissions.toArray(new String[permissions.size()]), requestCode);
+    }
+
+    public static boolean addPermissionList(Activity activity, List<String> listPermission, String permission) {
+        if (ActivityCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+            listPermission.add(permission);
+            // Check show rationale
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(activity, permission))
+                return false;
+        }
+        return true;
+    }
 }
